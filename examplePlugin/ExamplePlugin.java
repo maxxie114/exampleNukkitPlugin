2d3a026b6e7eb132d03d700984d847e4ad1e104d
maxxie114 <maxxie114@gmail.com>
2018-11-05 13:05:07 -0800

    change config configuration to new method

--- examplePlugin/ExamplePlugin.java
+++ examplePlugin/ExamplePlugin.java
@@ -1,41 +1,36 @@
 package examplePlugin;
 
-import java.io.File;
-import java.util.LinkedHashMap;
 import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandSender;
 import cn.nukkit.plugin.PluginBase;
-import cn.nukkit.utils.Config;
 import cn.nukkit.utils.TextFormat;
 
 public class ExamplePlugin extends PluginBase {
 
   private String msg;
-  private Config config;
+  private int number;
+  // private Config config;
 
-  @SuppressWarnings("deprecation")
+  // @SuppressWarnings("deprecation")
   @Override
   public void onEnable() {
     this.getLogger().info(TextFormat.GREEN + "ExamplePlugin enabled.");
     this.getServer().getPluginManager().registerEvents(new EventListener(this), this);
-
-    this.config = new Config(new File(this.getDataFolder(), "config.yml"), Config.YAML,
-
-        new LinkedHashMap<String, Object>() {
-          /**
-           * 
-           */
-          private static final long serialVersionUID = 1L;
-
-          // create key spawnRange with default input 900
-          {
-            // default settings specific to 2b2tbe
-            put("commandMessage", "Welcome!");
-          }
-        });
-
-    msg = String.valueOf(config.get("commandMessage"));
-
+    this.saveDefaultConfig();
+    msg = String.valueOf(this.getConfig().get("commandMessage"));
+    number = this.getConfig().getInt("Number");
+    /*
+     * this.config = new Config(new File(this.getDataFolder(), "config.yml"), Config.YAML,
+     * 
+     * new LinkedHashMap<String, Object>() {
+     * 
+     * private static final long serialVersionUID = 1L;
+     * 
+     * // create key spawnRange with default input 900 { // default settings specific to 2b2tbe
+     * put("commandMessage", "Welcome!"); } });
+     * 
+     * msg = String.valueOf(config.get("commandMessage"));
+     */
   }
 
   @Override
@@ -46,19 +41,25 @@ public class ExamplePlugin extends PluginBase {
 
   @Override
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    // String name = command.getName();
     if (command.getName().equalsIgnoreCase("example")) {
       String message = msg;
       sender.sendMessage(TextFormat.BLUE + message);
+      int num = number;
+      sender.sendMessage(TextFormat.GREEN + Integer.toString(num));
+      return true;
+    } else if (command.getName().equalsIgnoreCase("getnum")) {
+      int num = number;
+      sender.sendMessage(TextFormat.GREEN + Integer.toString(num));
       return true;
     } else {
       sender.sendMessage(TextFormat.RED + "Command doesn't exist.");
-      return true;
     }
-
+    return false;
   }
 
   /** For eclipse compiler */
   public static void main(String[] Args) {
-    System.out.println("Compiled.");
+    System.out.println("Compiled!");
   }
 }
